c4b30621994c60fe759010b4b6b4ef13e19c5ac0
mueenamueen <mueenamueen786@gmail.com>
2024-10-16 16:17:33 +0530

    mapping

---
+++ src/main/java/lk/ijse/gdse68/springpossystembackend/util/AppUtil.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse68.springpossystembackend.util;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class AppUtil {
+    public static  String createCustomerId(){
+        return "CUS-"+ UUID.randomUUID();
+    }
+    public static String createItemCode(){
+        return "ITM-"+UUID.randomUUID();
+    }
+    public static String createOrderId(){
+        return "ORD-"+UUID.randomUUID();
+    }
+    public static String createOrderDetailsId(){
+        return "ODI-"+UUID.randomUUID();
+    }
+
+    public static LocalDate getCurrentDate() {
+        return LocalDate.now();
+    }
+}
