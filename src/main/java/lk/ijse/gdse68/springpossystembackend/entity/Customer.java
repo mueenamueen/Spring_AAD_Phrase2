aeecc74e4a0fac15b1abd2fa249581324bbfdcab
mueenamueen <mueenamueen786@gmail.com>
2024-10-16 16:25:35 +0530

    create entites

---
+++ src/main/java/lk/ijse/gdse68/springpossystembackend/entity/Customer.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse68.springpossystembackend.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse68.springpossystembackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Customer")
+public class Customer implements SuperDTO {
+    @Id
+    @Column(name = "id", length = 50)
+    private String id;
+    @Column(name = "name", length = 50)
+    private String name;
+    @Column(name = "address", length = 50)
+    private String address;
+    @Column(name = "salary")
+    private Double salary;
+    @OneToMany(mappedBy = "customer",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Orders> orders;
+}
---
+++ src/main/java/lk/ijse/gdse68/springpossystembackend/entity/Item.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse68.springpossystembackend.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse68.springpossystembackend.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Item")
+@Entity
+public class Item implements SuperDTO {
+    @Id
+    private String code;
+    private String name;
+    private BigDecimal price;
+    private int qty;
+    @OneToMany(mappedBy = "item",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<OrderDetails> orderItems;
+}
