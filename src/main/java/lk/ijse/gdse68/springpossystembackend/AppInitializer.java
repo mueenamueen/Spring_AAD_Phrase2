10f03358934c784c75498ce81910333037040e8e
mueenamueen <mueenamueen786@gmail.com>
2024-10-16 16:18:08 +0530

    Appinitializer

---
+++ src/main/java/lk/ijse/gdse68/springpossystembackend/AppInitializer.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse68.springpossystembackend;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.gdse68.springpossystembackend.config.WebAppConfig;
+import lk.ijse.gdse68.springpossystembackend.config.WebAppConfigRoot;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppConfigRoot.class};
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
+    }
+}
